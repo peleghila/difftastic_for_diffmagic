f16fc64e8d9ab4e9be08b405a61d1ebffa324abc
Bitroix1 <noar_z6_2022@guttman.ort.org.il>
2025-06-26 12:45:59 +0300

    made bash running file use relative path

--- diffmagic.sh
+++ diffmagic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "▶ Running diffmagic(target/release/difft)"
-./target/release/difft /mnt/c/Users/Bitroix/Desktop/Technion/Diff/difftastic/Files/lhs /mnt/c/Users/Bitroix/Desktop/Technion/Diff/difftastic/Files/rhs > /mnt/c/Users/Bitroix/Desktop/Technion/Diff/difftastic/Files/result.txt
+./target/release/difft Files/lhs Files/rhs > Files/result.txt
 echo "✅ Saved to Files/result.txt"
 
 echo
 echo "▶ Running expected(bin/release/difft)"
-./bin/difft /mnt/c/Users/Bitroix/Desktop/Technion/Diff/difftastic/Files/lhs /mnt/c/Users/Bitroix/Desktop/Technion/Diff/difftastic/Files/rhs > /mnt/c/Users/Bitroix/Desktop/Technion/Diff/difftastic/Files/expected.txt
+./bin/release/difft Files/lhs Files/rhs > Files/expected.txt
 echo "✅ Saved to Files/expected.txt"
